7c0b5b80623c150a59b999365713d74a8fd10f6e
Omarr-Darwish <omar.mhamedsalem@gmail.com>
2025-10-02 00:42:42 +0300

    Add files via upload

---
+++ LEC1.1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a,b;
+    cout << "enter frist number:";
+    cin >> a;
+    cout << "enter second number:";
+    cin >> b;
+    cout << a + b ;
+    return 0;
+}
